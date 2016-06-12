95dfb73c7d1cb0d6d344b67851a61c1d766349c0
Gianni Portal <zagitt@gmail.com>
2016-06-12 03:25:03 +0000

    Improve Space page design

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -38,8 +38,7 @@ class SpacesController < ApplicationController
   # POST /spaces.json
   def create
     @space = Space.new(space_params)
-    @space.profile = current_user.default_profile
-    
+
     respond_to do |format|
       if @space.save
         format.html { redirect_to @space, notice: 'Space was successfully created.' }
